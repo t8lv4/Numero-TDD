2b80ca7162a3c3655e1ac6944fce83df38970cdd
t8lv4 <morgan@t8lv4.fr>
2018-08-28 11:38:29 +0200

    Test number 10
    add testConversionForTen()
    modify convert(_:)

--- Numero/Converter.swift
+++ Numero/Converter.swift
@@ -5,6 +5,10 @@ class Converter {
     func convert(_ number: Int) -> String {
         var result = ""
         var localNumber = number
+        if localNumber >= 10 {
+            result += "X"
+            localNumber = localNumber - 10
+        }
         if localNumber >= 5 {
             result += "V"
             localNumber = localNumber - 5
--- NumeroTests/ConverterTests.swift
+++ NumeroTests/ConverterTests.swift
@@ -41,4 +41,9 @@ class ConverterTests: XCTestCase {
         let result = converter.convert(6)
         XCTAssertEqual(result, "VI", "Conversion for 6 is incorrect")
     }
+
+    func testConversionForTen() {
+        let result = converter.convert(10)
+        XCTAssertEqual(result, "X", "Conversion for 10 is incorrect")
+    }
 }
