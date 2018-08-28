45dcb70cd5463996bc4759d38c5518d01a78d0c2
t8lv4 <morgan@t8lv4.fr>
2018-08-28 10:55:49 +0200

    Create ConverterTEsts.swift
    set up ConverterTests

---
+++ NumeroTests/ConverterTests.swift
@@ -0,0 +1,26 @@
+//
+//  ConverterTests.swift
+//  NumeroTests
+//
+//  Created by Morgan on 28/08/2018.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+import XCTest
+@testable import Numero
+
+class ConverterTests: XCTestCase {
+
+    let converter = Converter()
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+}
